fab506c1c273be5f836f053e53d37d893829f98f
Mo Zheng <michaelzheng@example.com>
2026-05-30 01:06:45 -0400

    Add Edit menu for Lucy chat copy paste

--- swift_app/Sources/main.swift
+++ swift_app/Sources/main.swift
@@ -1,7 +1,57 @@
 import Cocoa
 import Foundation
 
+
+func buildMainMenu() {
+    let mainMenu = NSMenu()
+
+    let appMenuItem = NSMenuItem()
+    mainMenu.addItem(appMenuItem)
+
+    let appMenu = NSMenu()
+    appMenuItem.submenu = appMenu
+    appMenu.addItem(
+        withTitle: "Quit Lucy",
+        action: #selector(NSApplication.terminate(_:)),
+        keyEquivalent: "q"
+    )
+
+    let editMenuItem = NSMenuItem()
+    mainMenu.addItem(editMenuItem)
+
+    let editMenu = NSMenu(title: "Edit")
+    editMenuItem.submenu = editMenu
+
+    editMenu.addItem(
+        withTitle: "Cut",
+        action: #selector(NSText.cut(_:)),
+        keyEquivalent: "x"
+    )
+
+    editMenu.addItem(
+        withTitle: "Copy",
+        action: #selector(NSText.copy(_:)),
+        keyEquivalent: "c"
+    )
+
+    editMenu.addItem(
+        withTitle: "Paste",
+        action: #selector(NSText.paste(_:)),
+        keyEquivalent: "v"
+    )
+
+    editMenu.addItem(
+        withTitle: "Select All",
+        action: #selector(NSText.selectAll(_:)),
+        keyEquivalent: "a"
+    )
+
+    NSApp.mainMenu = mainMenu
+}
+
+
 let app = NSApplication.shared
+buildMainMenu()
 let delegate = AppDelegate()
 app.delegate = delegate
 app.setActivationPolicy(.accessory)
